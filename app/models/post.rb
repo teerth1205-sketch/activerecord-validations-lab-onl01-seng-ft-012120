909ea4214f24aac94fcfb458927ec2d56cd03110
Teerth Patel <teerth1205@gmail.com>
2020-03-30 19:04:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates(:content, { :length => { :minimum => 250} })
   validates(:summary, { :length => { :maximum => 250} })
  validates :category, inclusion: { in: %w(Fiction Non-Fiction)} 
-   validate :is_clickbait
+   validate :is_clickbait()
 
  
  def is_clickbait(record)
