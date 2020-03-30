e47014c9e7983cfa93ec6be28614db04f9444dd1
Teerth Patel <teerth1205@gmail.com>
2020-03-30 18:11:54 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ActiveRecord::Base
    validates :name, presence: true
    validates :name, uniqueness: true
-   validates(:registration_number, { :length => { :is => 6 } })
+   validates(:phone_number, { :length => { :is => 10} })
+  
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :post presence :true 
+  validates(:post_content, { :length => { :minimum => 250} })
+  validates(:post_s, { :length => { :minimum => 250} })
 end
