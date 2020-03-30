e91a20918ce63beb24d7673275d377388ba3651e
Teerth Patel <teerth1205@gmail.com>
2020-03-30 18:12:14 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :post presence :true 
-  validates(:post_content, { :length => { :minimum => 250} })
-  validates(:post_s, { :length => { :minimum => 250} })
+  validates(:content, { :length => { :minimum => 250} })
+  validates(:summary, { :length => { :minimum => 250} })
 end
