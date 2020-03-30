f646ccec587761fd796414ff44243d6473014991
Teerth Patel <teerth1205@gmail.com>
2020-03-30 18:21:25 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :post presence: true 
   validates(:content, { :length => { :minimum => 250} })
   validates(:summary, { :length => { :maximum => 250} })
-  validates :category, inclusion: {in: "Fiction","Non-Fiction"} 
+ validates :category, inclusion: { in: %w(Fiction Non-Fiction)} 
 end
