48de791b988734bfdeb57f1d104eacc50b55d349
Teerth Patel <teerth1205@gmail.com>
2020-03-30 18:41:01 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
   validates(:content, { :length => { :minimum => 250} })
   validates(:summary, { :length => { :maximum => 250} })
  validates :category, inclusion: { in: %w(Fiction Non-Fiction)} 
+ 
+ @@phrases = [/won't believe/, /secret/, /top \d/, /guess/]
+ 
+ def isclickbaity
+  valid = false
+   if @@phrases.each {|phrase| self.title =~ phrase} 
+     valid = true
+   else 
+     errors[:title] << "Need a beter title"
+   end 
+   
+ end 
 end
