ee3c69f207529798cef34c5ac7c4827fd01921bf
Teerth Patel <teerth1205@gmail.com>
2020-03-30 19:05:13 +0000

    Automatically backed up by Learn

--- app/helpers/my_validator.rb
+++ app/helpers/my_validator.rb
@@ -1,5 +1,15 @@
 class MyValidator < ActiveModel::Validator
   
+ def is_clickbait(record)
+  reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+   if reqs.detect {|phrase| record.title.include?(phrase)}.nil?
+     record.errors[:title] << "Need a beter title"
+      
+   end 
+   
+ end 
+ 
+@@reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
   
   
   
